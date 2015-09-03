2f6d1e9b83126a0d35e82448c237aa24e2564172
tommy williams <tom@technomad.media>
2015-09-03 18:39:08 -0500

    set up
    everything working

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var ROOT_PATH = path.resolve(__dirname);
+
+module.exports = {
+  entry: path.resolve(ROOT_PATH, 'app/main'),
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: path.resolve(ROOT_PATH, 'app')
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'test_app2'
+    })
+  ]
+};
